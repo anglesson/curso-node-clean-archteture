2908d8d0e5a37892b29702885c24f5f8e6096c95
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-04-15 08:59:30 -0300

    refactor: reorganize folders

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,4 +1,4 @@
-import { LogErrorRepository } from '../../data/usecases/add-account/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
 import { ok, serverError } from '../../presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
