6cfc7ed77b3e13f5d315829464aa3e57cf4948c4
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-04-15 20:55:41 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -11,10 +11,11 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<any> {
-    if (!httpRequest.body.email) {
+    const { email, password } = httpRequest.body
+    if (!email) {
       return await Promise.resolve(badRequest(new MissingParamError('email')))
     }
-    if (!httpRequest.body.password) {
+    if (!password) {
       return await Promise.resolve(badRequest(new MissingParamError('password')))
     }
     const isValid = this.emailValidator.isValid(httpRequest.body.email)
