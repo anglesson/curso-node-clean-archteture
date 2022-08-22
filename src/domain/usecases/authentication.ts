ecc53bc1259bbe01178091894238318fb96486f4
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-08-21 23:20:58 -0300

    refactor: change authentication to receive a model

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,3 +1,8 @@
+export interface authenticationModel {
+  email: string
+  password: string
+}
+
 export interface Authentication {
-  auth (email: string, password: string): Promise<string>
+  auth (authentication: authenticationModel): Promise<string>
 }
